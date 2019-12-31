71efe704c5a11f98d1b46a303df7bc9b6cfa462c
טל ששון <tals@h-mbp-sl-tl.home>
2019-12-31 09:20:22 +0200

    Updates

---
+++ precache-manifest.983ec8fd532f8767f8f81d1879550956.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d472a98f2c0c82df134b5d7f9486e07c",
+    "url": "/Tal-Sasson-27-12-2019/index.html"
+  },
+  {
+    "revision": "30b32a3f48d13db24cfb",
+    "url": "/Tal-Sasson-27-12-2019/static/css/main.5c1dc5d9.chunk.css"
+  },
+  {
+    "revision": "59284e4be16b09e95433",
+    "url": "/Tal-Sasson-27-12-2019/static/js/2.1b2731dd.chunk.js"
+  },
+  {
+    "revision": "b9bc2fc7725df2ce4b7409c91212bc51",
+    "url": "/Tal-Sasson-27-12-2019/static/js/2.1b2731dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30b32a3f48d13db24cfb",
+    "url": "/Tal-Sasson-27-12-2019/static/js/main.43ce1cbf.chunk.js"
+  },
+  {
+    "revision": "3aca74a5ea1ac937b5a3",
+    "url": "/Tal-Sasson-27-12-2019/static/js/runtime-main.6a352325.js"
+  },
+  {
+    "revision": "e7be0b470dc83dd8d169e0278ce1da11",
+    "url": "/Tal-Sasson-27-12-2019/static/media/bg.e7be0b47.jpeg"
+  }
+]);
